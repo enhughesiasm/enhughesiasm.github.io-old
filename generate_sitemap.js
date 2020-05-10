80b61329794d5db137fa5bc292c9d952b8a18345
Neil Hughes <enhughesiasm@googlemail.com>
2020-05-10 12:39:52 +0100

    Tweaked copy on contact page

--- generate_sitemap.js
+++ generate_sitemap.js
@@ -13,10 +13,12 @@ const paramsConfig = {};
 
 const Routes = require('./src/components/routes/routes_sitemap').default;
 
+console.log('\x1b[33m%s\x1b[0m', 'Beginning sitemap.'); // 33 = yellow
+
 new Sitemap(Routes)
 	.filterPaths(filterConfig)
 	.applyParams(paramsConfig)
 	.build('https://enhughesiasm.com', { limitCountPaths: 5000 })
 	.save('./public/sitemap.xml', 'static');
 
-console.log('Sitemap generation code completed.');
+console.log('\x1b[32m%s\x1b[0m', 'Sitemap generation code completed.'); // 32 = green
--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -3,20 +3,33 @@ import HelmetContainer from '../shared/helmet_container';
 import ContactFormGate from './contact_form_gate';
 
 function Contact() {
-
-	return (<div className="has-text-centered has-spacing-top-3">
-		<HelmetContainer page="contact"/>
-		<h1 className="title">contact</h1>
-		<div className="content">
-			<div className="notification is-light">
-				<p>Email is welcome! Use the form below, or email directly:</p>
-				<p><em>neil <strong>at</strong> walkingoncustard <strong>dot</strong> com</em></p>
-				<p className="notification is-primary">I read every email and reply to as many as I can.</p>
+	return (
+		<div className='has-text-centered has-spacing-top-3'>
+			<HelmetContainer page='contact' />
+			<h1 className='title'>contact</h1>
+			<div className='content'>
+				<div className='notification is-light'>
+					<p>Email is welcome!</p>
+					<p>
+						<strong>Use the form below</strong>, or email directly:
+					</p>
+					<p>
+						<em>
+							neil <strong>at</strong> walkingoncustard{' '}
+							<strong>dot</strong> com
+						</em>
+					</p>
+					<p className='notification is-primary'>
+						I read every email and reply to as many as I can.
+					</p>
+				</div>
+				<ContactFormGate />
+				<p className='is-size-6 notification is-light'>
+					Or use your preferred social media site via the links below.
+				</p>
 			</div>
-			<ContactFormGate/>
-			<p className="is-size-6 notification is-light">Or use your preferred social media site via the links below.</p>
 		</div>
-	</div>);}
-
+	);
+}
 
 export default Contact;
