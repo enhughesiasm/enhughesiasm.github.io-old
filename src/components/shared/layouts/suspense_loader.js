bca57941879f0a6944b1708b1ed9ce8045fafe31
Neil Hughes <enhughesiasm@googlemail.com>
2020-06-01 09:46:19 +0100

    Polished the loading display

--- src/components/shared/layouts/suspense_loader.js
+++ src/components/shared/layouts/suspense_loader.js
@@ -2,8 +2,19 @@ import React from 'react';
 import LoadingBounce from '../loading_bounce';
 
 const SuspenseLoader = () => (
-	<div style={{ width: '100%', textAlign: 'center' }}>
-		<LoadingBounce size='100' />
+	<div
+		className='notification is-light is-flex'
+		style={{
+			height: '50vh',
+			width: '100%',
+			textAlign: 'center',
+			justifyContent: 'center',
+			alignItems: 'center',
+			display: 'flex',
+		}}>
+		<div style={{ justifyContent: 'center', alignItems: 'center' }}>
+			{/* Loading... <LoadingBounce size='100' /> */}
+		</div>
 	</div>
 );
 
